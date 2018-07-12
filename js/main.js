6d366c6195426333e933297e15efde48e1e0fe24
irenemherrero <irenemherrero@gmail.com>
2018-07-12 09:39:08 +0200

    add design changes

--- js/main.js
+++ js/main.js
@@ -8,10 +8,10 @@ function searchFilms() {
   var inputContent = input.value;
   var url = 'http://api.tvmaze.com/search/shows?q=';
   fetch(url + inputContent)
-    .then(function (response) {
+    .then(function(response) {
       return response.json();
     })
-    .then(function (responseJson) {
+    .then(function(responseJson) {
       var ul = document.createElement('ul');
       ul.classList.add("list");
       containerResults.innerHTML = '';
@@ -24,23 +24,23 @@ function searchFilms() {
         li.classList.add('container-no-favorite');
         title.classList.add('film-title');
         title.appendChild(titleContent);
-        li.appendChild(title);
         image.classList.add('image-film');
-        if (responseJson[i].show.image === null) {
+        if(responseJson[i].show.image === null) {
           image.src = 'https://via.placeholder.com/210x295/cccccc/666666/?text=TV';
         } else {
           image.src = responseJson[i].show.image.medium;
         }
         li.append(image);
+        li.appendChild(title);
         li.addEventListener("click", changeFavorite);
         ul.appendChild(li);
       }
     });
 }
 
-function changeFavorite(e){
+function changeFavorite(e) {
   var li = e.currentTarget;
-  if(li.classList.contains("container-no-favorite")){
+  if(li.classList.contains("container-no-favorite")) {
     li.classList.toggle("container-favorite")
   } else {
     li.classList.delete("container-favorite")
